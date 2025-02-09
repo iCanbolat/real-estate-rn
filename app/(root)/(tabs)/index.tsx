865cb610d287f687a7f5eda91f0a925484ca0936
iCanbolat <fmc_canbolat@hotmail.com>
2025-02-09 14:18:18 +0300

    feat: Database & Collection & API setup

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,12 +1,55 @@
-import { View, Text } from "react-native";
+import { View, Text, Image, TouchableOpacity } from "react-native";
 import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { useGlobalContext } from "@/lib/global-provider";
+import { useLocalSearchParams } from "expo-router";
+import icons from "@/constants/icons";
+import Search from "@/components/search";
 
-const Test = () => {
+const Index = () => {
+  const { user } = useGlobalContext();
+
+  const params = useLocalSearchParams<{ query?: string; filter?: string }>();
+
   return (
-    <View>
-      <Text>Test</Text>
-    </View>
+    <SafeAreaView className="h-full bg-white">
+      <View className="px-5">
+        <View className="flex flex-row items-center justify-between mt-5">
+          <View className="flex flex-row">
+            <Image
+              source={{ uri: user?.avatar }}
+              className="size-12 rounded-full"
+            />
+
+            <View className="flex flex-col items-start ml-2 justify-center">
+              <Text className="text-xs font-rubik text-black-100">
+                Good Morning
+              </Text>
+              <Text className="text-base font-rubik-medium text-black-300">
+                {user?.name}
+              </Text>
+            </View>
+          </View>
+          <Image source={icons.bell} className="size-6" />
+        </View>
+
+        <Search />
+        
+        <View className="my-5">
+          <View className="flex flex-row items-center justify-between">
+            <Text className="text-xl font-rubik-bold text-black-300">
+              Featured
+            </Text>
+            <TouchableOpacity>
+              <Text className="text-base font-rubik-bold text-primary-300">
+                See all
+              </Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
+    </SafeAreaView>
   );
 };
 
-export default Test;
+export default Index;
